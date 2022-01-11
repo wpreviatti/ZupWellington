134ec1a7ca14924e58198b6a9b97eadd9c6669a7
Wellington o lindo <wpreviatti@outlook.com>
2022-01-11 19:10:23 -0200

    Ultimos topicos que deu tempo de fazer

---
+++ EFClient.Teste/Services/UserServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EFClient.Repo;
+using EFClient.WebApi.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EFClient.Teste.Services
+{
+    public class ClientServices
+    {
+        private ClienteController clienteController;
+        public ClientServices()
+        {
+            //clienteController = new ClienteController(new Mock<IEFCoreRepository>().Object, new Mock<IMapper>().Object);
+        }
+    }
+}
