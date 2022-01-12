0fe452af28293590164c25b1817280ef5fc1762a
Wellington o lindo <wpreviatti@outlook.com>
2022-01-12 10:58:49 -0200

    Enviado banco

---
+++ BANCO/dbo/Tables/Cliente.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Cliente] (
+    [NumeroChapaId]          INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]                   NVARCHAR (MAX) NOT NULL,
+    [Sobrenome]              NVARCHAR (MAX) NOT NULL,
+    [Mail]                   NVARCHAR (MAX) NOT NULL,
+    [NomeLiderNumeroChapaId] INT            NULL,
+    [Senha]                  NVARCHAR (MAX) NOT NULL,
+    [DtCadastro]             DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED ([NumeroChapaId] ASC),
+    CONSTRAINT [FK_Cliente_Cliente_NomeLiderNumeroChapaId] FOREIGN KEY ([NomeLiderNumeroChapaId]) REFERENCES [dbo].[Cliente] ([NumeroChapaId])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Cliente_NomeLiderNumeroChapaId]
+    ON [dbo].[Cliente]([NomeLiderNumeroChapaId] ASC);
+
---
+++ BANCO/dbo/Tables/Telefone.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Telefone] (
+    [Id]                   INT            IDENTITY (1, 1) NOT NULL,
+    [NumeroTelefone]       NVARCHAR (MAX) NOT NULL,
+    [TipoTelefone]         NVARCHAR (MAX) NULL,
+    [ClienteNumeroChapaId] INT            NULL,
+    CONSTRAINT [PK_Telefone] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Telefone_Cliente_ClienteNumeroChapaId] FOREIGN KEY ([ClienteNumeroChapaId]) REFERENCES [dbo].[Cliente] ([NumeroChapaId])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Telefone_ClienteNumeroChapaId]
+    ON [dbo].[Telefone]([ClienteNumeroChapaId] ASC);
+
